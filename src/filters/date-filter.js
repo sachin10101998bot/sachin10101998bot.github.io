1533c05b4d4ba37ee0ad018a478a9b30f438a298
sachin10101998 <schopra@mt.iitr.ac.in>
2020-05-28 12:55:29 +0530

    U

--- src/filters/date-filter.js
+++ src/filters/date-filter.js
@@ -1,16 +1,17 @@
-
-// Stolen from https://stackoverflow.com/a/31615643
+/* eslint-env es6 */
+/* eslint-disable no-console */
+"use strict";
+// this function is strict...
 const appendSuffix = n => {
-    var s = ['th', 'st', 'nd', 'rd'],
-      v = n % 100;
+    var s = ["th", "st", "nd", "rd"],
+        v = n % 100;
     return n + (s[(v - 20) % 10] || s[v] || s[0]);
-  };
-  
-  module.exports = function dateFilter(value) {
+};
+module.exports = function dateFilter(value) {
     const dateObject = new Date(value);
-  
-    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     const dayWithSuffix = appendSuffix(dateObject.getDate());
-  
+
     return `${dayWithSuffix} ${months[dateObject.getMonth()]} ${dateObject.getFullYear()}`;
-  };
+};
